0d8014e99b4b96f55b9b4aeaaa2093e02823acd9
Cagatay <cagataybayramgonen@gmail.com>
2021-10-27 23:11:26 +0300

    day12 end

--- com.Batch30TestNG/src/test/java/tests/day12/C01_WaitTest.java
+++ com.Batch30TestNG/src/test/java/tests/day12/C01_WaitTest.java
@@ -1,5 +1,12 @@
 package tests.day12;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.TestBase;
 
 public class C01_WaitTest extends TestBase {
@@ -13,5 +20,28 @@ public class C01_WaitTest extends TestBase {
     6. Add buttonuna basin
     7. It’s back mesajinin gorundugunu test edin
      */
+    @Test
+    public void implicitlyWaitTest(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(driver.findElement(By.id("message")).isDisplayed(),"mesaj goruntulenmiyor");
+        softAssert.assertAll();
+        driver.findElement(By.xpath("//*[text()='Add']")).click();
+        Assert.assertTrue(driver.findElement(By.id("message")).isDisplayed(),"mesaj goruntulenmiyor");
+    }
+    @Test
+    public void explicitlyWaitTest(){
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        WebElement withTextLocate = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(withTextLocate.isDisplayed(),"mesaj goruntulenmiyor");
+        softAssert.assertAll();
+        driver.findElement(By.xpath("//*[text()='Add']")).click();
+        WebElement addMessageLocate = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        Assert.assertTrue(addMessageLocate.isDisplayed(),"mesaj goruntulenmiyor");
+    }
 
 }
---
+++ com.Batch30TestNG/src/test/java/tests/day12/C02_Faker.java
@@ -0,0 +1,67 @@
+package tests.day12;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C02_Faker extends TestBase {
+    /*
+      "https://facebook.com"  Adresine gidin
+“create new account”  butonuna basin
+“firstName” giris kutusuna bir isim yazin
+“surname” giris kutusuna bir soyisim yazin
+“email” giris kutusuna bir email yazin
+“email” onay kutusuna emaili tekrar yazin
+Bir sifre girin
+Tarih icin gun secin
+Tarih icin ay secin
+Tarih icin yil secin
+Cinsiyeti secin
+Isaretlediginiz cinsiyetin secili, diger cinsiyet kutusunun secili olmadigini test edin.
+Sayfayi kapatin
+
+     */
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://facebook.com");
+        driver.findElement(By.linkText("Yeni Hesap Oluştur")).click();
+        Faker faker = new Faker();
+        String email = faker.internet().emailAddress();
+        Actions actions = new Actions(driver);
+        WebElement firsNameBox = driver.findElement(By.xpath("//input[@name='firstname']"));
+        actions.click(firsNameBox)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(String.valueOf(faker.number().numberBetween(1,31)))
+                .sendKeys(Keys.TAB)
+                .sendKeys("May")
+                .sendKeys(Keys.TAB)
+                .sendKeys(String.valueOf(faker.number().numberBetween(1,31)))
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .sendKeys(Keys.ARROW_LEFT)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .perform();
+        Thread.sleep(5000);
+        Assert.assertTrue(driver.findElement(By.xpath("(//input[@name='sex'])[1]")).isSelected(),"Cinsiyet secilmedi");
+        actions.sendKeys(Keys.ENTER).perform();
+    }
+}
